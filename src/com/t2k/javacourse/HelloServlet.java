8931586dc9bcb9fda4f4058c18faf729a3291323
derrevere <derrevere@gmail.com>
2014-12-22 14:05:39 +0200

    commit math 1

--- src/com/t2k/javacourse/HelloServlet.java
+++ src/com/t2k/javacourse/HelloServlet.java
@@ -5,7 +5,20 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class HelloServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		//variable declaration
+		int num1;
+		int num2;
+		int num3;
+		//variable assignment
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		//add calculations here
+		int result = (num1 + num2) * num3;
+		//define a string to include all calculations:
+		String resultStr = new String("<h1>Result of ("+num1+"+"+num2+")*"+num3
+				+"="+result+"</h1>");
+		resp.getWriter().println(resultStr);
 	}
 }
